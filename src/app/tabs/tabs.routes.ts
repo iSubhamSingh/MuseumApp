33ae841658a006f65e5efd2c12082ddf1f19455d
Subham <subhamsingh5638@gmail.com>
2023-06-20 21:55:44 +0530

    Initial commit

--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
         path: 'tab1',
         loadComponent: () =>
           import('../tab1/tab1.page').then((m) => m.Tab1Page),
-      },
+      },  
       {
         path: 'tab2',
         loadComponent: () =>
@@ -20,6 +20,11 @@ export const routes: Routes = [
         path: 'tab3',
         loadComponent: () =>
           import('../tab3/tab3.page').then((m) => m.Tab3Page),
+      },
+      {
+        path: 'tab4',
+        loadComponent: () =>
+          import('../tab4/tab4.page').then((m) => m.Tab4Page),
       },
       {
         path: '',
